75446432fb165a2bbd5b4aa1cc51279358d10152
David Johansson <david.u.johansson+github@gmail.com>
2014-12-13 07:57:20 +0100

    initial

---
+++ src/main/java/se/orbilius/GoodStuff.java
@@ -0,0 +1,12 @@
+package se.orbilius;
+
+public class GoodStuff{
+	public long id;
+	public String name;
+
+	public GoodStuff(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
---
+++ src/main/java/se/orbilius/HttpCachingController.java
@@ -0,0 +1,23 @@
+package se.orbilius;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.WebContentGenerator;
+
+@RestController
+public class HttpCachingController extends WebContentGenerator{
+
+    private final AtomicLong counter = new AtomicLong();
+
+	
+	@RequestMapping("/hello")
+	public GoodStuff hello(HttpServletResponse response) {
+		response.setHeader("Cache-Control", "max-age=10");
+		return new GoodStuff(counter.getAndIncrement(), "a name");
+	}
+
+}
